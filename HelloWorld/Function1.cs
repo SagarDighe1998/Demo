24607d8262b4d774db4683e4c777259aa58e331d
Sagar Dighe <sagardighe9022@gmail.com>
2023-08-31 18:33:17 +0530

    First commit

---
+++ HelloWorld/Function1.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+public static class HelloWorldFunction
+{
+    [FunctionName("GreetFunction")] public static async Task<IActionResult> 
+        Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] 
+    HttpRequest req, ILogger log) { log.LogInformation("C# HTTP trigger function processed a request.");
+        string name = req.Query["name"];
+        string responseMessage = string.IsNullOrEmpty(name) ? "Please provide a name in the query string." : $"Hello, {name}!";
+        return new OkObjectResult(responseMessage); }
+}
+
